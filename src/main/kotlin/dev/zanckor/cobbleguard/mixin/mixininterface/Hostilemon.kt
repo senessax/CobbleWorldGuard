15385efa26c365668ae0017d6e4f774ed66e4650
Enric <Zanckor002@gmail.com>
2025-01-26 18:09:48 +0100

    Added method to calculate the best movement based on effectiveness on Hostilemon.

--- src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixininterface/Hostilemon.kt
+++ src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixininterface/Hostilemon.kt
@@ -1,13 +1,17 @@
 package dev.zanckor.cobbleguard.mixin.mixininterface
 
 import com.cobblemon.mod.common.api.moves.Move
+import com.cobblemon.mod.common.api.types.ElementalType
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.pokemon.Pokemon
 import net.minecraft.world.entity.LivingEntity
 
 interface Hostilemon {
     var isHostile: Boolean
 
-    val randomMove: Move?
+    fun getBestMove(): Move?
 
     fun useMove(move: Move?, target: LivingEntity?)
-    fun attack(target: LivingEntity?, damage: Double)
+
+    fun getMoveEffectiveness(move: Move?, targetType: ElementalType): Double
 }

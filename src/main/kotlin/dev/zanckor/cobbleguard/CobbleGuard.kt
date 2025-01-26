716830192cae8ca6806a113c70e368139da32c82
Enric <Zanckor002@gmail.com>
2025-01-26 19:37:32 +0100

    Added MODID val to CobbleGuard

--- src/main/kotlin/dev/zanckor/cobbleguard/CobbleGuard.kt
+++ src/main/kotlin/dev/zanckor/cobbleguard/CobbleGuard.kt
@@ -1,9 +1,16 @@
 package dev.zanckor.cobbleguard
 
+import dev.zanckor.cobbleguard.core.brain.registry.PokemonSensors
 import net.fabricmc.api.ModInitializer
 
 class CobbleGuard : ModInitializer {
 
     override fun onInitialize() {
+        PokemonSensors.init()
+    }
+
+    companion object {
+        @JvmField
+        var MODID: String = "cobbleguard"
     }
 }

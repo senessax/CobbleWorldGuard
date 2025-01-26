a5b9497e0c44f3c6d5b6324cf4ce8ce4f205c439
Enric <Zanckor002@gmail.com>
2025-01-26 19:36:23 +0100

    Changed getBestMove to getBestMoveAgainst adding the target which poke is aiming to attack

--- src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixininterface/Hostilemon.kt
+++ src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixininterface/Hostilemon.kt
@@ -2,14 +2,12 @@ package dev.zanckor.cobbleguard.mixin.mixininterface
 
 import com.cobblemon.mod.common.api.moves.Move
 import com.cobblemon.mod.common.api.types.ElementalType
-import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
-import com.cobblemon.mod.common.pokemon.Pokemon
 import net.minecraft.world.entity.LivingEntity
 
 interface Hostilemon {
     var isHostile: Boolean
 
-    fun getBestMove(): Move?
+    fun getBestMoveAgainst(target: LivingEntity?): Move?
 
     fun useMove(move: Move?, target: LivingEntity?)
 

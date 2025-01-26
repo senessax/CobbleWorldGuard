6d34416e63c94e026d3648dcae0eab8b436edccf
Enric <Zanckor002@gmail.com>
2025-01-26 18:10:17 +0100

    Implemented Hostilemon.kt on PokemonMixin

--- src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixin/PokemonMixin.kt
+++ src/main/kotlin/dev/zanckor/cobbleguard/mixin/mixin/PokemonMixin.kt
@@ -1,7 +1,10 @@
 package dev.zanckor.cobbleguard.mixin.mixin
 
 import com.cobblemon.mod.common.api.moves.Move
+import com.cobblemon.mod.common.api.types.ElementalType
+import com.cobblemon.mod.common.battles.ai.typeEffectiveness
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.pokemon.Pokemon
 import dev.zanckor.cobbleguard.mixin.mixininterface.Hostilemon
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
@@ -10,22 +13,50 @@ import net.minecraft.world.level.Level
 import net.tslat.smartbrainlib.api.SmartBrainOwner
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor
 import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.Shadow
 
 @Mixin(PokemonEntity::class)
 class PokemonMixin(entityType: EntityType<out PathfinderMob>, level: Level,
-                   override var isHostile: Boolean,
-                   override val randomMove: Move?
+                   override var isHostile: Boolean
 ) :
     PathfinderMob(entityType, level),
     Hostilemon,
     SmartBrainOwner<PokemonMixin> {
 
-    override fun useMove(move: Move?, target: LivingEntity?) {
+    @Shadow
+    private val pokemon: Pokemon? = null
+    override fun getBestMove(): Move? {
+        val isPokemon = target is PokemonEntity
 
+        if(isPokemon) {
+            val pokemonTarget = (target as PokemonEntity).pokemon
+            val moves = pokemon!!.moveSet.map {
+                val primaryTypeEffectiveness = getMoveEffectiveness(it, pokemonTarget.primaryType)
+                val secondaryTypeEffectiveness = if(pokemonTarget.secondaryType != null) getMoveEffectiveness(it, pokemonTarget.secondaryType!!) else 0.0
+
+                Triple(it, primaryTypeEffectiveness + secondaryTypeEffectiveness, it.power)
+            }.sortedBy { it.second + it.third }
+
+            return moves.last().first
+        } else {
+            return pokemon!!.moveSet[random.nextIntBetweenInclusive(0, 3)]
+        }
     }
 
-    override fun attack(target: LivingEntity?, damage: Double) {
+    override fun useMove(move: Move?, target: LivingEntity?) {
+        if(move != null) {
+            val damage = move.power * getMoveEffectiveness(move, (target as PokemonEntity).pokemon.primaryType)
+            target.hurt(damageSources().mobAttack(this), damage.toFloat())
+        }
+    }
 
+    override fun getMoveEffectiveness(move: Move?, targetType: ElementalType): Double {
+        val type = move!!.type
+        typeEffectiveness[type]?.let {
+            return it[targetType] ?: 0.0
+        }
+
+        return 0.0
     }
 
     override fun getSensors(): MutableList<out ExtendedSensor<out PokemonMixin>> {
